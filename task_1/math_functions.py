907140faf883ea20cdae8230a7c09b1450857a0f
HolidayMan <lesha.pitenko791@gmail.com>
2019-09-19 00:21:57 +0300

    created calculate_move_vector function

--- task_1/math_functions.py
+++ task_1/math_functions.py
@@ -1,10 +1,17 @@
-from math import hypot
+from math import hypot, ceil
 import csv
+from collections import namedtuple
+
+
+Point = namedtuple('Point', ['x', 'y', 'color'])
 
 class Vector:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def __repr__(self):
+        return f'<Vector(x={self.x}, y={self.y})>'
 
     def __abs__(self):
         return hypot(self.x, self.y)
@@ -13,16 +20,18 @@ class Vector:
         return bool(self.x or self.y)
 
     def __add__(self, other):
+        if type(other) == Point:
+            return Point(other.x+self.x, other.y+self.y)
         x = self.x + other.x
         y = self.y + other.y
         return Vector(x, y)
 
+    def __truediv__(self, scalar):
+        return Vector(self.x / scalar, self.y / scalar)
+
     def __mul__(self, scalar):
-        return Vector(self.x * scalar, self.y * scalar)
+        return Vector(int(self.x * scalar), int(self.y * scalar))
 
-
-    def __div__(self, scalar):
-        return Vector(self.x // scalar, self.y // scalar)
 
 def export_coordinates(coordinates):
     with open('coordinates.csv', 'w') as file:
@@ -30,3 +39,8 @@ def export_coordinates(coordinates):
         writer.writerow(('x', 'y'))
         for point in coordinates:
             writer.writerow((str(point.x), str(point.y)))
+
+
+def calculate_move_vector(v, px):
+    v_dots_amount = abs(v) / px
+    return (v / v_dots_amount, int(v_dots_amount))
